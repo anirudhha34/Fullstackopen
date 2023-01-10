d1ed686d04e34a31804576267e699038738cdaa2
anirudhha34 <59689450+anirudhha34@users.noreply.github.com>
2023-01-10 23:24:14 +0530

    Add files via upload

---
+++ part1/Unicafe/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const StatisticsLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + bad + neutral;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+  return all > 0 ? (
+    <>
+      <p>
+        <b>statistic</b>
+      </p>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={all} />
+          <StatisticsLine text="average" value={average} />
+          <StatisticsLine text="positive" value={positive} />
+        </tbody>
+      </table>
+    </>
+  ) : (
+    <p>No feedback given</p>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increment = (state, setstate) => () => setstate(state + 1);
+  return (
+    <div>
+      <p>
+        <b>Give feedback</b>
+      </p>
+      <Button onClick={increment(good, setGood)} text="good" />
+      <Button onClick={increment(neutral, setNeutral)} text="neutral" />
+      <Button onClick={increment(bad, setBad)} text="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  );
+};
+
+export default App;
